3b0cf60aa20ab973b870dece196054a3f0083be1
ezgideren <ezgi_deren@yahoo.com>
2024-07-01 20:43:18 +0200

    User model added

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,3 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import User from '../models/user';
 
 const router = express.Router();
+
+router.post("/register", async (req: Request, res: Response) => {
+    try {
+        //check user model, find any documents where the email matches the email in the request body
+        let user = await User.findOne({email:req.body.email});
+
+        if (user) {
+            return res.status(400).json({message: "User already exists"});
+        }
+        user = new User(req.body);
+        await user.save();
+ 
+    } catch (error) {
+        res.status(500).send({message: "Something went wrong"})
+    }
+});
